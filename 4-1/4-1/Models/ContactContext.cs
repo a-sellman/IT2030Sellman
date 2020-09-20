de5b11ae465a4faf8eed780579d3f4a68a700935
a-sellman <44267652+a-sellman@users.noreply.github.com>
2020-09-20 14:41:57 -0400

    ....

--- 4-1/4-1/Models/ContactContext.cs
+++ 4-1/4-1/Models/ContactContext.cs
@@ -27,9 +27,9 @@ namespace _4_1.Models
 
                 );
             modelBuilder.Entity<Contact>().HasData(
-                new Contact { CategoryID = 1, FName = "Jane", LName = "Doe", PhoneNumber = 999-999-9999, Email = "janedoe@gmail.com" },
-                new Contact { CategoryID = 2, FName = "John", LName = "Doe", PhoneNumber = 111 - 111 - 1111, Email = "johndoe@gmail.com" },
-                new Contact { CategoryID = 3, FName = "Mary", LName ="Johnson", PhoneNumber = 222 - 222 - 2222, Email = "maryjohnson@gmail.com" }
+                new Contact { CategoryID = 1, FName = "Jane", LName = "Doe", PhoneNumber = 9999, Email = "janedoe@gmail.com" },
+                new Contact { CategoryID = 2, FName = "John", LName = "Doe", PhoneNumber = 1111, Email = "johndoe@gmail.com" },
+                new Contact { CategoryID = 3, FName = "Mary", LName ="Johnson", PhoneNumber = 2222, Email = "maryjohnson@gmail.com" }
             );
         }
     }
