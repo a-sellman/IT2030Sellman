3fbceda65702f6a12c6ce1af5ecb575ea37a89f7
a-sellman <44267652+a-sellman@users.noreply.github.com>
2020-10-11 10:42:04 -0400

    file structure and controller progress

---
+++ 7-1/7-1/Controllers/TutorialController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using _7_1.Models;
+
+namespace _7_1.Controllers
+{
+    public class TutorialController : Controller
+    {
+        private readonly ILogger<TutorialController> _logger;
+
+        public TutorialController(ILogger<TutorialController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Page1"); //Views/Tutorial/Page1.cshtml
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
