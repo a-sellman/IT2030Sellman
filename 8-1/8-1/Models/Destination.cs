0d150af1179aed5287fdcb90b98b6f75375f8ce5
a-sellman <44267652+a-sellman@users.noreply.github.com>
2020-10-17 20:56:18 -0400

    Final Touches on HTMl pages, preparing for database code

--- 8-1/8-1/Models/Destination.cs
+++ 8-1/8-1/Models/Destination.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace _8_1.Models
 {
@@ -25,4 +27,44 @@ namespace _8_1.Models
         [Key]
         public string TripID { get; set; }
     }
+
+    /*   public void Button1_Click(object sender, EventArgs e)
+
+      {
+          string TripID = TripID.Text.ToString();
+          string AccomodationName = AccomodationName.Text.ToString();
+          string AccomodationEmail = AccomodationEmail.Text.ToString();
+          string AccomodationPhone = AccomodationPhone.Text.ToString();
+          string ThingToDo1 = ThingToDo1.Text.ToString();
+          string ThingToDo2 = ThingToDo2.Text.ToString();
+          string ThingToDo3 = ThingToDo3.Text.ToString();
+          string StartDate = StartDate.Text.ToString();
+          string EndDate = EndDate.Text.ToString();
+
+          try
+          {
+              using (var conn = new SqlConnection())
+              {
+                  conn.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                  conn.Open();
+
+                  var insertCommand = new SqlCommand("INSERT INTO [Destination] (TripID, AccomodationName, AccomodationEmail, AccomodationPhone, ThingToDo1, ThingToDo2, ThingToDo3, StartDate, EndDate) VALUES (@TripID, @AccomodationName, @AccomodationEmail, @AccomodationPhone, @StartDate, @EndDate, @ThingToDo1, @ThingToDo2, @ThingToDo3); ", conn);
+
+                  insertCommand.Parameters.Add(new SqlParameter("TripID", TripID));
+                  insertCommand.Parameters.Add(new SqlParameter("StartDate", StartDate));
+                  insertCommand.Parameters.Add(new SqlParameter("EndDate", EndDate));
+                  insertCommand.Parameters.Add(new SqlParameter("AccomodationEmail", AccomodationEmail));
+                  insertCommand.Parameters.Add(new SqlParameter("AccomodationPhone", AccomodationPhone));
+                  insertCommand.Parameters.Add(new SqlParameter("AccomodationName", AccomodationName));
+                  insertCommand.ExecuteNonQuery();
+                  conn.Close();
+                  Page.ClientScript.RegisterStartupScript(this.GetType(), "close", "<script language=javascript>window.opener.location.reload(true);self.close();</script>");
+              }
+          }
+          catch (Exception ex)
+          {
+              throw ex;
+          }
+      }
+    */
 }
