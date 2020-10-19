545805fd86a0720bf496bb8b6ae00140296aa282
a-sellman <44267652+a-sellman@users.noreply.github.com>
2020-10-18 21:48:48 -0400

    almost done

--- 8-1/8-1/Controllers/HomeController.cs
+++ 8-1/8-1/Controllers/HomeController.cs
@@ -2,16 +2,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
 
 namespace _8_1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DestinationContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DestinationContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [HttpGet]
@@ -21,11 +25,54 @@ namespace _8_1.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Accomodations(Destination request)
+        {
+            _logger.LogTrace($"Received request{request}.");
+            if (ModelState.IsValid)
+            {
+                return View(request);
+            }
+            _context.Update(request);
+            await _context.SaveChangesAsync();
+            return new ViewResult()
+            {
+                ViewName = "ThingsToDo",
+            };
+        }
+
         [HttpGet]
         public ActionResult Destination(int id, string role)
 
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Destination(Destination request)
+        {
+            _logger.LogTrace($"Received request{request}.");
+            if (ModelState.IsValid)
+            {
+                return View(request);
+            }
+
+            if (_context.Destination.FindAsync(request.TripID) == null)
+            {
+                _context.Add(request);
+            }
+            else
+            {
+                _context.Update(request);
+            }
+            await _context.SaveChangesAsync();
+
+            return View("Accomodations", request);
+            //return new ViewResult()
+            //{
+            //    ViewName = "Accomodations",
+
+            //};
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -42,6 +89,22 @@ namespace _8_1.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ThingsToDo(Destination request)
+        {
+            _logger.LogTrace($"Received request{request}.");
+            if (ModelState.IsValid)
+            {
+                return View(request);
+            }
+            _context.Update(request);
+            await _context.SaveChangesAsync();
+            return new ViewResult()
+            {
+                ViewName = "Index",
+            };
         }
 
         [HttpGet]
--- 8-1/8-1/Models/Destination.cs
+++ 8-1/8-1/Models/Destination.cs
@@ -20,51 +20,13 @@ namespace _8_1.Models
         public int StartDate { get; set; }
 
         public string ThingToDo1 { get; set; }
+
         public string ThingToDo2 { get; set; }
+
         public string ThingToDo3 { get; set; }
 
         [Required(ErrorMessage = "Please Enter A Destination.")]
         [Key]
         public string TripID { get; set; }
     }
-
-    /*   public void Button1_Click(object sender, EventArgs e)
-
-      {
-          string TripID = TripID.Text.ToString();
-          string AccomodationName = AccomodationName.Text.ToString();
-          string AccomodationEmail = AccomodationEmail.Text.ToString();
-          string AccomodationPhone = AccomodationPhone.Text.ToString();
-          string ThingToDo1 = ThingToDo1.Text.ToString();
-          string ThingToDo2 = ThingToDo2.Text.ToString();
-          string ThingToDo3 = ThingToDo3.Text.ToString();
-          string StartDate = StartDate.Text.ToString();
-          string EndDate = EndDate.Text.ToString();
-
-          try
-          {
-              using (var conn = new SqlConnection())
-              {
-                  conn.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
-                  conn.Open();
-
-                  var insertCommand = new SqlCommand("INSERT INTO [Destination] (TripID, AccomodationName, AccomodationEmail, AccomodationPhone, ThingToDo1, ThingToDo2, ThingToDo3, StartDate, EndDate) VALUES (@TripID, @AccomodationName, @AccomodationEmail, @AccomodationPhone, @StartDate, @EndDate, @ThingToDo1, @ThingToDo2, @ThingToDo3); ", conn);
-
-                  insertCommand.Parameters.Add(new SqlParameter("TripID", TripID));
-                  insertCommand.Parameters.Add(new SqlParameter("StartDate", StartDate));
-                  insertCommand.Parameters.Add(new SqlParameter("EndDate", EndDate));
-                  insertCommand.Parameters.Add(new SqlParameter("AccomodationEmail", AccomodationEmail));
-                  insertCommand.Parameters.Add(new SqlParameter("AccomodationPhone", AccomodationPhone));
-                  insertCommand.Parameters.Add(new SqlParameter("AccomodationName", AccomodationName));
-                  insertCommand.ExecuteNonQuery();
-                  conn.Close();
-                  Page.ClientScript.RegisterStartupScript(this.GetType(), "close", "<script language=javascript>window.opener.location.reload(true);self.close();</script>");
-              }
-          }
-          catch (Exception ex)
-          {
-              throw ex;
-          }
-      }
-    */
 }
