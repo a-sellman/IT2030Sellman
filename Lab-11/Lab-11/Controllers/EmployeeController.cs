f84b0bf09f11427139d6fda5f240ce7e5ca03618
a-sellman <44267652+a-sellman@users.noreply.github.com>
2020-12-03 16:01:16 -0500

    further validation work and controllers

--- Lab-11/Lab-11/Controllers/EmployeeController.cs
+++ Lab-11/Lab-11/Controllers/EmployeeController.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Lab_11.Models;
 
 namespace Lab_11.Controllers
 {
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public EmployeeController(SalesContext ctx) => context = ctx;
+
+        private SalesContext context { get; set; }
+        public IActionResult
     }
 }
--- Lab-11/Lab-11/Models/Validation/Validate.cs
+++ Lab-11/Lab-11/Models/Validation/Validate.cs
@@ -7,5 +7,35 @@ namespace Lab_11.Models.Validation
 {
     public class Validate
     {
+        public static string CheckEmployee(SalesContext context, Employee employee)
+        {
+            Employee searchEmployee = context.Employee.FirstOrDefault(
+                e => e.FirstName.ToLower() == employee.FirstName.ToLower()
+                && e.LastName.ToLower() == employee.LastName.ToLower()
+                && e.DateOfBirth == employee.DateOfBirth
+            );
+
+            return searchEmployee == null ?
+                string.Empty
+                : $"{searchEmployee.FullName} (DOB: {searchEmployee.DateOfBirth?.ToShortDateString()}) already exists.";
+        }
+
+        public static string CheckManagerEmployeeMatch(SalesContext context, Employee employee)
+        {
+            Employee manager = context.Employee.Find(employee.ManagerId);
+            if (manager != null
+                && manager.FirstName == employee.FirstName
+                && manager.LastName == employee.LastName
+               && manager.DateOfBirth == employee.DateOfBirth)
+            {
+                return $"Manager and employee cannot be the same";
+            }
+            return string.Empty;
+        }
+
+        public static string CheckSales(SalesContext context, Sales sale)
+        {
+            throw new NotFiniteNumberException();
+        }
     }
 }
