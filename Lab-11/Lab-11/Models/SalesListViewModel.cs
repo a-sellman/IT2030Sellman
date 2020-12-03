2f12bf1e47d398701073a81e680d95429e445b5e
a-sellman <44267652+a-sellman@users.noreply.github.com>
2020-12-03 15:08:35 -0500

    working on index pages

---
+++ Lab-11/Lab-11/Models/SalesListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_11.Models
+{
+    public class SalesListViewModel
+    {
+        public int EmployeeId { get; set; }
+        public List<Employee> Employees { get; set; }
+        public List<Sales> Sales { get; set; }
+    }
+}
