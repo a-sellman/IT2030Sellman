772ac2ee31cf83382dad4a5166eebe86cbab36d2
a-sellman <44267652+a-sellman@users.noreply.github.com>
2020-12-13 11:27:40 -0500

    removing razor code from layout

---
+++ 15-1_Blackjack/BlackJack/Models/TagHelpers/TempMessageTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace BlackJack.Models
+{
+    [HtmlTargetElement("my-temp-message")]
+    public class TempMessageTagHelper : TagHelper
+    {
+        [ViewContext]
+        [HtmlAttributeNotBoundAttribute]
+        public ViewContext ViewCtx { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var tempdata = ViewCtx.TempData;
+
+            if (tempdata.ContainsKey("message"))
+            {
+                string background = tempdata["background"]?.ToString() ?? "info";
+                output.BuildTag("h4", $"bg-{background} text-center text-white p-2");
+                output.Content.SetContent(tempdata["message"].ToString());
+            }
+            else
+            {
+                output.SuppressOutput();
+            }
+        }
+    }
+}
